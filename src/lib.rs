e34f246a6a6fc867f131a9c5c472a549cbb4edcf
Jorge Arturo Martínez Sánchez <jorgearturo@ciencias.unam.mx>
2022-05-21 18:35:06 -0500

    Euler method for ODE of order n added. Also three tests were made

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
 pub use self::mesh::Mesh;
 
 mod mesh;
+
+// PRIMER EJERCICIO
+/// Euler's method for ordinary differential equations.
+/// The form of the equation is assumed to be "y'(n) = f(t,y,y',..,y'(n-1))".
+/// 
+/// # Parameters
+/// - initial_val: Vec<f32>
+///   The previous values of every variable. The derivatives go in descending order: [y'(n),y'(n-1),...,t]
+/// - step: f32
+///   The step used to calculate the approximation.
+/// - f: T where T: Fn(Vec<f32>) -> f32
+///   Reffers to the function on the left side of the above equation.
+/// 
+/// # Returns
+/// - Vec<f32>
+///   A vector of values corresponding to the new approximation. The derivatives are delivered as in 'initial_val'
+/// 
+pub fn euler_step<T: Fn(&Vec<f32>) -> f32>(mut initial_val: Vec<f32>, step: f32, f: T) -> Vec<f32> {
+    let f_eval: f32 = f(&initial_val);
+    let mut next_values: Vec<f32> = vec![];
+    
+    let mut value: f32 = initial_val.get(0).unwrap() + step*f_eval;
+    let t_new: f32 = initial_val.get(initial_val.len()-1).unwrap() + step;
+    
+    next_values.push(value);
+
+    initial_val[1..initial_val.len()-1].into_iter().for_each(|x| {
+        let new_val: f32 = x + step*value;
+        value = new_val;
+        next_values.push(new_val);
+    });
+    next_values.push(t_new);
+
+    next_values
+}
---
+++ tests/euler.rs
@@ -0,0 +1,62 @@
+use Dzahui::euler_step;
+
+#[test]
+fn first_order_ODE() {
+    let constant_velocity = |initial_val: &Vec<f32>| {1.0};
+    let step = 0.01;
+    let mut time: f32 = 0.0;
+    let mut pos: f32 = 0.0; // m
+    while time <= 10.0 {
+        if let [_pos,_time] = &euler_step(vec![pos,time], step, constant_velocity)[..] {
+            pos = *_pos;
+            time = *_time;
+        }; 
+    }
+    assert_eq!(pos<=10.1,true);
+    assert_eq!(pos>=9.9,true);
+}
+
+#[test]
+fn second_order_ODE_gravity() {
+    let gravity: f32 = 9.81; // m/s^2   
+    let force_g = |_val: &Vec<f32>| -gravity;
+    let step: f32 = 0.01;
+    let mut time: f32 = 0.0;
+    let mut pos: f32 = 100.0; // m
+    let mut vel: f32 = 0.0; // m/s
+    while time <= 10.0 { 
+        if let [_vel,_pos,_time] = &euler_step(vec![vel,pos,time], step, force_g)[..] {
+            vel = *_vel;
+            pos = *_pos;
+            time = *_time;
+        };
+    }
+    assert_eq!(vel>=-100.0,true);
+    assert_eq!(vel<=-97.0,true);
+    assert_eq!(pos>=-400.0,true);
+    assert_eq!(pos<=-389.0,true);
+}
+
+#[test]
+fn radioactive_decay_Radium_ODE() {
+    let half_life: f32 = 1600.0; //years
+    let radioactive_decay_constant: f32 = 2.0_f32.ln()/half_life;
+    let decay_function = |val: &Vec<f32>| {
+        -radioactive_decay_constant*val.get(0).unwrap() // -lambda * N
+    };
+    let step: f32 = 0.5; // years
+    let mut time: f32 = -1600.0; // years
+    let mut quantity: f32 = 1000.0; // parent nuclei
+    while time <= 0.0 {
+        if let [_quantity,_time] = &euler_step(vec![quantity,time], step, decay_function)[..] {
+            quantity = *_quantity;
+            time = *_time;
+        }
+    }
+
+    assert_eq!(time<=0.5,true);
+    assert_eq!(time>=-0.5,true);
+    assert_eq!(quantity>=490.0,true);
+    assert_eq!(quantity<=510.0,true);
+
+}
