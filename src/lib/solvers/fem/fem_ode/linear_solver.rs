8bfaa13753800b1d431cf7bfbbaa4713127e8325
Jorge Arturo Martínez Sánchez <jorgearturo@ciencias.unam.mx>
2022-08-19 10:17:46 -0500

    Modules also documented. Some traits still require changes regarding the signature of ther methods due to lack of parameters to make them work or more functions to access these parameters

--- src/lib/solvers/fem/fem_ode/linear_solver.rs
+++ src/lib/solvers/fem/fem_ode/linear_solver.rs
@@ -1,4 +1,19 @@
+/// # General Information
+/// 
+/// Represents a solver of a linear problem (master problem if you will) of the form **Ax=b** in which **A** is a square matrix, **b** 
+/// is a known vector and **x** is to be found.
+/// It needs to implement a solver function that returns the desired result.
+/// 
 trait LinearSolver {
+    
+    /// # General Information
+    /// 
+    /// A function that solves a problem with te information present in each struct implementing this trait.
+    /// 
+    /// # Parameters
+    /// 
+    /// 
+    /// 
     fn solve_mat() -> Vec<f32>;
 }
 
--- src/lib/solvers/fem/fem_ode/quadrature.rs
+++ src/lib/solvers/fem/fem_ode/quadrature.rs
@@ -1,5 +1,21 @@
 
+/// # General Information
+/// 
+/// A quadrature represents a process to obtain per-interval areas that (naturally) summed can approximate an integral. The current input consits on a vector
+/// of vectors of functions, each that represents a derivate of a given set of functions (commonly a basis), but probably will be changed in the future allowing
+/// to only provide a vector of functions each wit a trait representing differentiability.
+///  
 trait Quadrature1D {
+    
+    /// # General Information
+    /// 
+    /// Integrates a given set of functions according to a solver's instructions.
+    /// 
+    /// # Parameters
+    /// 
+    /// * `functions` - a vector of vector of functions, better described (mathematically) as a vector of a function set and all the necessary derivatives of such
+    /// a set.
+    /// 
     fn integrate(functions: Vec<Vec<Box<dyn Fn(f32) -> f32>>>) -> f32;
 }
 trait NewtonCotes: Quadrature1D {}
