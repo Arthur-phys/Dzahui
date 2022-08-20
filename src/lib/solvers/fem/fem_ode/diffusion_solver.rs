1474472dbe22b9c35e67e1173526ee816c55989d
Jorge Arturo Martínez Sánchez <jorgearturo@ciencias.unam.mx>
2022-08-19 22:00:43 -0500

    Second attempt to implement diffusion solver. Seems to be working witrh general enough structs and traits. Only creating function basis

--- src/lib/solvers/fem/fem_ode/diffusion_solver.rs
+++ src/lib/solvers/fem/fem_ode/diffusion_solver.rs
@@ -12,144 +12,219 @@
 //     }
 // }
 
-// struct DiffussionSolver {
-//     boundary_vertices: [BoundaryVertex1D; 2],
-//     vertices: Vec<Vertex1D>,
-// }
-
-// impl DiffEquationSolver for DiffussionSolver {
-//     fn solve() -> Vec<f32> {
-//         todo!()
-//     }
-// }
-
-struct Function<'a> {
-    function: Box<dyn Fn(f32) -> f32>,
-    derivatives: Vec<Box<dyn Fn(f32) -> f32>>,
-    composed: Vec<&'a Function<'a>>,
-    c_class: usize
+trait Function {
+    fn evaluate(&self, x: f32) -> f32;
 }
 
-impl<'a> Function<'a> {
-    fn new(function: Box<dyn Fn(f32) -> f32>, mut derivatives: Vec<Box<dyn Fn(f32) -> f32>>) -> Self {
-        
-        derivatives.reverse();
-        let c_class = derivatives.len();
+trait Affine: Function {
+    fn compose(&self, other: Self) -> Self;
+}
 
+trait Differentiable: Function {
+    fn differentiate(&self) -> Box<dyn Function>;
+}
+
+struct FirstDegreePolynomial {
+    coefficient: f32,
+    independent_term: f32,
+}
+
+impl FirstDegreePolynomial {
+    
+    fn new(coefficient: f32, independent_term: f32) -> Self {
         Self {
-            function,
-            derivatives,
-            c_class,
-            composed: vec![]
+            coefficient,
+            independent_term
         }
     }
 
-    fn new_from_composition(function: Box<dyn Fn(f32) -> f32>, mut derivatives: Vec<Box<dyn Fn(f32) -> f32>>, refs: Vec<&'a Function>) -> Self {
-        
-        derivatives.reverse();
-        let c_class = derivatives.len();
-        
+    fn zero() -> Self {
         Self {
-            function,
-            derivatives,
-            c_class,
-            composed: refs
+            coefficient: 0_f32,
+            independent_term: 0_f32
         }
     }
 
-    fn function(&self, x: f32) -> f32 {
-        let f = &self.function;
-        f(x)
+    fn constant(independent_term: f32) -> Self {
+        Self {
+            coefficient: 0_f32,
+            independent_term
+        }
     }
 }
 
-trait Differentiable {
-    fn differentiate<'a>(&'a self, n: usize) -> Option<&'a Box<dyn Fn(f32) -> f32>>;
-}
 
-
-impl<'b> Differentiable for Function<'b> {
-    fn differentiate<'a>(&'a self, n: usize) -> Option<&'a Box<dyn Fn(f32) -> f32>> {
-
-        self.derivatives.get(n-1)
-
+impl Function for FirstDegreePolynomial {
+    
+    fn evaluate(&self, x: f32) -> f32 {
+        self.coefficient * x + self.independent_term
     }
+
 }
 
-struct LinearBasis<'a> {
-    unit_basis: [Function<'a>; 2],
-}
-
-impl<'a> LinearBasis<'a> {
-
-    fn transformation(start: f32, finish: f32) -> Function<'a> {
-        Function::new(
-            Box::new(move |x: f32| {
-                        (x - start) / (finish - start)
-                    }),
-            vec![
-                Box::new(move |x: f32| {
-                    start / (finish - start)
-                })
-            ]
+impl Differentiable for FirstDegreePolynomial {
+    
+    fn differentiate(&self) -> Box<dyn Function> {
+        Box::new(
+            FirstDegreePolynomial::constant(self.coefficient)
         )
     }
 
-    fn create_basis(self, mesh: Vec<f32>) -> Vec<Function<'a>> {
-        
-        let phi_1 = &self.unit_basis[0];
-        let phi_2: &'a Function = &self.unit_basis[1];
+}
 
-        let mut basis_vector: Vec<Function> = Vec::with_capacity(mesh.len());
+impl Affine for FirstDegreePolynomial {
+    fn compose(&self, other: Self) -> Self {
+        Self {
+            coefficient: self.coefficient * other.coefficient,
+            independent_term: self.coefficient * other.independent_term + self.independent_term
+        }
+    }
+}
 
-        mesh.iter().zip(mesh.iter().skip(1)).enumerate().for_each(|(pos,(start,finish))| {
+trait IntervalStep {}
+trait NumberOfArguments {}
 
-            let Function {function: transformation, derivatives, ..} = LinearBasis::transformation(*start,*finish);
+impl IntervalStep for [f32;3] {}
+impl NumberOfArguments for [f32;4] {}
 
-            let phi_2_ref = phi_2.clone();
-            // if pos == 0 {
-                let interval_func = Function::new_from_composition( Box::new(move |x: f32| {
-                    phi_2_ref.function(transformation(x))
-                }),vec![], vec![phi_2_ref]);
+struct PiecewiseFirstDegreePolynomial<A: IntervalStep, B: NumberOfArguments> {
+    coefficients: B,
+    independent_terms: B,
+    interval: A
+}
 
-                basis_vector.push(interval_func)
+impl PiecewiseFirstDegreePolynomial<[f32;3],[f32;4]> {
+    
+    fn new(coefficients: [f32;4], independent_terms: [f32;4], interval: [f32;3]) -> Self {
+        Self {
+            coefficients,
+            independent_terms,
+            interval
+        }
+    }
 
-            // } else { }
-                
-            //     let interval_func = Function::new(Box::new(|x: f32| {
-            //         if x > *start && x < *finish {
-                    
-            //             phi_2.function(transformation.function(x))
-                    
-            //         } else if x < *start && x > *&mesh[pos-1] {
+    fn constants(independent_terms: [f32;4], interval: [f32;3]) -> Self {
+        Self {
+            coefficients: [0_f32;4],
+            independent_terms,
+            interval  
+        }
+    }
 
-            //             phi_1.function(transformation.function(x))
-                    
-            //         } else {
-
-            //             0_f32
-            //         }
-            //     }),
-            //     vec![Box::new(|x: f32| {
-            //         if x > *start && x < *finish {
-                    
-            //             phi_2.function(transformation.function(x))
-                    
-            //         } else if x < *start && x > mesh[pos-1] {
-
-            //             phi_1.function(transformation.function(x))
-                    
-            //         } else {
-
-            //             0_f32
-            //         }
-            //     })]);
-            
-            // }
-        });
-
-        basis_vector
-
+    fn from_polynomials(functions: [&FirstDegreePolynomial;4],interval: [f32;3]) -> Self {
+        Self {
+            coefficients: [functions[0].coefficient,functions[1].coefficient,
+            functions[2].coefficient,functions[3].coefficient],
+            independent_terms: [functions[0].independent_term,functions[1].independent_term,
+            functions[2].independent_term,functions[3].independent_term],
+            interval
+        }
     }
 
 }
+
+impl Function for PiecewiseFirstDegreePolynomial<[f32;3],[f32;4]> {
+    
+    fn evaluate(&self, x: f32) -> f32 {
+        if x < self.interval[0] {
+            self.coefficients[0] * x + self.independent_terms[0]
+        } else if x >= self.interval[0] && x < self.interval[1] {
+            self.coefficients[1] * x + self.independent_terms[1]
+        } else if x >= self.interval[1] && x < self.interval[2] {
+            self.coefficients[2] * x + self.independent_terms[2]
+        } else {
+            self.coefficients[3] * x + self.independent_terms[3]
+        }
+    }
+
+}
+
+impl Differentiable for PiecewiseFirstDegreePolynomial<[f32;3],[f32;4]> {
+    
+    fn differentiate(&self) -> Box<dyn Function> {
+        Box::new(
+            PiecewiseFirstDegreePolynomial::constants(self.independent_terms, self.interval)
+        )
+    }
+
+}
+
+struct TransformationFactory();
+
+impl TransformationFactory {
+
+    fn build(&self, beg: f32, end: f32) -> FirstDegreePolynomial {
+        let coefficient = 1_f32 / (end - beg);
+        let independent_term = -1_f32 / (end - beg); 
+        FirstDegreePolynomial { coefficient, independent_term }
+    }
+
+}
+
+struct LinearBasis {
+    basis: Vec<FirstDegreePolynomial>,
+    transformation: TransformationFactory
+}
+
+struct PiecewiseLinearBasis<A: IntervalStep, B: NumberOfArguments> {
+    basis: Vec<PiecewiseFirstDegreePolynomial<A,B>>,
+    interval: Vec<f32>,
+}
+
+impl<A: IntervalStep, B: NumberOfArguments> PiecewiseLinearBasis<A, B> { 
+    fn new(basis: Vec<PiecewiseFirstDegreePolynomial<A,B>>, interval: Vec<f32>) -> Self {
+        Self {
+            basis,
+            interval
+        }
+    }
+}
+
+impl LinearBasis {
+
+    fn new() -> Self {
+        let phi_1 = FirstDegreePolynomial::new(1_f32,0_f32);
+        let phi_2 = FirstDegreePolynomial::new(-1_f32,1_f32);
+        Self {
+            basis: vec![phi_1,phi_2],
+            transformation: TransformationFactory()
+        }
+    }
+
+    fn transform_basis(self, mesh: Vec<f32>) -> PiecewiseLinearBasis<[f32;3],[f32;4]> {
+
+        let phi_1 = &self.basis[0];
+        let phi_2= &self.basis[1];
+
+        let zero = FirstDegreePolynomial::zero();
+
+        let transformation = self.transformation.build(mesh[0],mesh[1]);
+        let mut basis_transform = phi_1.compose(transformation);
+
+        let basis_vec = vec![
+            PiecewiseFirstDegreePolynomial::from_polynomials([&zero,&zero,&basis_transform,&zero], [mesh[0]-1_f32,mesh[0],mesh[1]])
+        ];
+
+        mesh.iter().zip(mesh.iter().skip(1)).zip(mesh.iter().skip(2)).for_each(|((prev, cur), next)| {
+
+        });
+
+        PiecewiseLinearBasis::new(basis_vec,mesh)
+    }
+} 
+
+struct DiffussionSolver {
+    boundary_conditions: [f32; 2],
+    vertices: Vec<f32>,
+}
+
+impl DiffussionSolver {
+
+    fn new(boundary_conditions: [f32; 2], vertices: Vec<f32>) -> Self {
+        Self {
+            boundary_conditions,
+            vertices
+        }
+    }
+}
+
