0fc2ea2da1df4bbdcdd87498e6418c128f9928f6
Jorge Arturo Martínez Sánchez <jorgearturo@ciencias.unam.mx>
2022-12-01 18:36:59 -0600

    Check view matrix generation for 2d and 3d meshes. It seems to be working wrongfuly

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -26,7 +26,10 @@ fn main() {
 
 
     /* MESH VIEW */
-    let window_builder: DzahuiWindowBuilder = DzahuiWindow::builder("./assets/sphere.obj").with_mesh_in_3d();
+    // 3D MESH
+    // let window_builder: DzahuiWindowBuilder = DzahuiWindow::builder("./assets/sphere.obj").with_mesh_in_3d();
+    // 2D MESH
+    let window_builder: DzahuiWindowBuilder = DzahuiWindow::builder("./assets/big_mesh.obj");
 
     let window = window_builder.build();
     window.run();
